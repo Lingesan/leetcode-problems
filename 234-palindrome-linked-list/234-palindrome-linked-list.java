ef43c586c4d0966ac121c91bb8a87f843fcf0eeb
Lingesan S <s.lingesan17@gmail.com>
2022-05-25 17:17:45 +0530

    Time: 18 ms (31.35%), Space: 103.2 MB (29.79%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -0,0 +1,37 @@
+import java.util.Deque;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null)
+            return false;
+        if (head.next == null)
+            return true;
+        Deque<Integer> stack = new LinkedList<>();
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        while (!stack.isEmpty()) {
+            if (Objects.equals(stack.pop(), slow.val))
+                slow = slow.next;
+            else
+                return false;
+        }
+        return true;
+        
+    }
+}
