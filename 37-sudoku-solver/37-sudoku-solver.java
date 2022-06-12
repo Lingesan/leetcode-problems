ca4f369573d81f8325c18a3e0297c89ea33ec39e
Lingesan S <s.lingesan17@gmail.com>
2022-06-12 21:00:13 +0530

    Time: 15 ms (56.86%), Space: 41.3 MB (64.85%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,75 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solveSudokuRecursive(board);
+    }
+    
+    private boolean solveSudokuRecursive(char[][] board){
+        for(int i = 0; i< 9;i++){
+            for(int j = 0; j < 9;j++){
+                if(board[i][j] != '.'){
+                    continue;
+                }
+                for(char k='1';k<='9';k++){
+                    char val = k;
+                    if(isValidCharacter(board,i,j,val)){
+                        board[i][j] = val;
+                        if(solveSudokuRecursive(board)){
+                            return true ;
+                        } else {
+                            board[i][j] = '.';
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidCharacter(char[][] board, int i, int j, char val){
+        return isValidRow(board[i], val) && isValidColumn(board,j, val) && isValidSubBox(board,i,j,val);
+    }
+    
+    private boolean isSolved(char[][] board){
+        for(char[] row: board){
+            for(char cell: row){
+                if(cell == '.'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidRow(char[] row, char value){
+        for(char cell: row){
+            if(cell == value){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidColumn(char[][] board,int columnIndex, char value){
+        
+        for(char[] row: board){
+            if(row[columnIndex] == value){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidSubBox(char[][] board, int rowIndex, int columnIndex, char value){
+        int k = rowIndex / 3;
+        int l = columnIndex / 3;
+        for(int i = k*3; i< k*3 + 3;i++){
+            for( int j = l * 3; j < l*3 +3;j++){
+                if(board[i][j] == value){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
